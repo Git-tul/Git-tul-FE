4babcc5842112fcf2428527933f9be4411261bc9
WooGi1020 <jungwk1020@gmail.com>
2025-03-08 19:25:34 +0900

    feat: 자잘한 수정사항 반영

--- src/app/(home)/post/_components/PostCard/index.tsx
+++ src/app/(home)/post/_components/PostCard/index.tsx
@@ -1,18 +1,66 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Switch } from "@/components/ui/switch";
+import PostCardContent from "@/app/(home)/post/_components/PostCard/_components/PostCardContent";
+import PostCardHeader from "@/app/(home)/post/_components/PostCard/_components/PostCardHeader";
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter } from "@/components/ui/card";
+import { Form } from "@/components/ui/form";
+import { postSchema } from "@/schema/postSchema";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import PreviewCard from "@/app/(home)/post/_components/PostCard/_components/PreviewCard";
+import { useState } from "react";
+import { Dialog } from "@/components/ui/dialog";
+import { DialogTrigger } from "@radix-ui/react-dialog";
 
 export default function PostCard() {
+  const postCardForm = useForm<z.infer<typeof postSchema>>({
+    resolver: zodResolver(postSchema),
+    defaultValues: {
+      aiSummation: false,
+      release: false,
+      title: "",
+      content: "",
+      tags: [],
+      reference: "",
+    },
+    mode: "onSubmit",
+  });
+
+  const handleSubmitPost = (data: z.infer<typeof postSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <Card className="w-full h-fit max-w-[736px] flex flex-col gap-4">
-      <CardHeader>
-        <div className="flex gap-2 items-center">
-          <p>AI 요약 생성</p>
-          <Switch />
-        </div>
-        <CardContent className="flex flex-col"></CardContent>
-      </CardHeader>
-    </Card>
+    <FormProvider {...postCardForm}>
+      <Dialog>
+        <Card className="w-full h-fit min-w-[700px]">
+          <Form {...postCardForm}>
+            <form onSubmit={postCardForm.handleSubmit(handleSubmitPost)} className=" flex flex-col gap-10">
+              <PostCardHeader control={postCardForm.control} />
+              <PostCardContent control={postCardForm.control} />
+              <CardFooter>
+                <div className="ml-auto flex gap-2">
+                  <DialogTrigger asChild>
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      className="border-1 p-5 hover:bg-muted-foreground hover:text-white"
+                    >
+                      미리보기
+                    </Button>
+                  </DialogTrigger>
+                  <Button type="submit" variant="default" className="border-1 p-5 bg-primary hover:bg-primary/90">
+                    발행하기
+                  </Button>
+                </div>
+              </CardFooter>
+            </form>
+          </Form>
+        </Card>
+        <PreviewCard />
+      </Dialog>
+    </FormProvider>
   );
 }
--- src/app/(home)/post/page.tsx
+++ src/app/(home)/post/page.tsx
@@ -2,7 +2,7 @@ import PostCard from "@/app/(home)/post/_components/PostCard";
 
 export default function Post() {
   return (
-    <section className="flex flex-1 gap-8 mt-12 max-w-[1248px] w-full mx-auto py-8 px-6 max-[1200px]:max-w-[930px]">
+    <section className="flex-1 flex gap-8 mt-12 max-w-[1248px] w-full mx-auto py-8 px-6">
       <div className="flex flex-col gap-8 w-full">
         <h1 className="text-2xl">카드 발행</h1>
         <PostCard />
